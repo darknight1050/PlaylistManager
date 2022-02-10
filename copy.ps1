fe2ef8f568936c07adf1eb1d16e92c79c52c5b8a
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-02-10 16:55:03 -0400

    Bump to 1.19 libs

--- copy.ps1
+++ copy.ps1
@@ -1,4 +1,4 @@
 & ./build.ps1
-& adb push libs/arm64-v8a/libplaylistmanager.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libplaylistmanager.so
+& adb push build/libplaylistmanager.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libplaylistmanager.so
 & adb shell am force-stop com.beatgames.beatsaber
 & adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
