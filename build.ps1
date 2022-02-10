780cc319fc983ed2ca621e9726dbb0c2904da183
darknight1050 <darkdeveloping1050@gmail.com>
2022-02-10 22:24:06 +0100

    Fix some stuff

--- build.ps1
+++ build.ps1
@@ -21,5 +21,5 @@ if (($clean.IsPresent) -or (-not (Test-Path -Path "build")))
 
 cd build
 & cmake -G "Ninja" -DCMAKE_BUILD_TYPE="RelWithDebInfo" ../
-& cmake --build .
+& cmake --build . -j 8
 cd ..
--- copy.ps1
+++ copy.ps1
@@ -1,4 +1,12 @@
 & ./build.ps1
-& adb push build/libplaylistmanager.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libplaylistmanager.so
+if ($useDebug.IsPresent) {
+    & adb push build/debug/libplaylistmanager.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libplaylistmanager.so
+} else {
+    & adb push build/libplaylistmanager.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libplaylistmanager.so
+}
+
 & adb shell am force-stop com.beatgames.beatsaber
 & adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
+if ($log.IsPresent) {
+    & ./log.ps1
+}
