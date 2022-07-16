9279d60fa2dc7015201afb1d342a5ca5a235b507
darknight1050 <darkdeveloping1050@gmail.com>
2022-07-16 15:16:02 +0200

    Update to 1.24.0

--- build.ps1
+++ build.ps1
@@ -12,13 +12,13 @@ if ($clean.IsPresent)
     }
 }
 
-
 if (($clean.IsPresent) -or (-not (Test-Path -Path "build")))
 {
     $out = new-item -Path build -ItemType Directory
 }
 
-cd build
-& cmake -G "Ninja" -DCMAKE_BUILD_TYPE="RelWithDebInfo" ../
-& cmake --build . -j 8
-cd ..
+& cmake -G "Ninja" -DCMAKE_BUILD_TYPE="RelWithDebInfo" -B build
+& cmake --build ./build -j 8
+$ExitCode = $LastExitCode
+
+exit $ExitCode
